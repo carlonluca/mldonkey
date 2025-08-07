a22c24c04db7e604a2186721ca97b99d7bdf16d7
Luca Carlon <carlon.luca@gmail.com>
2025-08-07 18:37:27 +0200

    Enable libcrypto++.

--- config/config.h
+++ config/config.h
@@ -1,3 +1,28 @@
+/** Copyright 2025 Luca Carlon
+
+    This file is part of mldonkey.
+
+    mldonkey is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    mldonkey is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mldonkey; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**/
+
+/*
+This file is a static placeholder used in dune builds. While the old
+make-based build was configurable, the new dune build is not, and
+everything is compiled in the final binary.
+*/
+
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
@@ -24,7 +49,7 @@
 #define HAVE_BZLIB_H 1
 
 /* cryptopp */
-/* #undef HAVE_CRYPTOPP */
+#define HAVE_CRYPTOPP 1
 
 /* Define to 1 if you have the <gd.h> header file. */
 #define HAVE_GD_H 1
