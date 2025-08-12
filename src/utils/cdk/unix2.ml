74042b68a3824d9580dbad849b3e46365a487572
Luca Carlon <carlon.luca@gmail.com>
2025-08-12 14:57:40 +0200

    Fixes.

--- src/utils/cdk/unix2.ml
+++ src/utils/cdk/unix2.ml
@@ -182,7 +182,7 @@ let rename oldname newname =
             Sys.remove newname
       
 external c_seek64 : Unix.file_descr -> int64 -> Unix.seek_command -> int64 =
-  "unix_lseek_64" 
+  "caml_unix_lseek_64" 
 external c_getsize64 : string -> int64 = "ml_getsize64"
 external c_getfdsize64 : Unix.file_descr -> int64 = "ml_getfdsize64"
 (* c_ftruncate64 sets size, optionally using a sparse file *)
